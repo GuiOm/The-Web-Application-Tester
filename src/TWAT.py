783228f5307a3cc6706bf46d87990c18ab278bb8
Guillaume Cheron <guillaum.cheron@gmail.com>
2012-01-23 23:09:05 +0100

    Ajout fichier TWAT.py

---
+++ src/TWAT.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys
+from GUI.FenetrePrincipale import *
+
+class TWAT:
+	
+	
+	
+	#Test le format de l'URL passé en argument
+	def testFormatURL(self, url):
+		pass
+	
+	#Test si l'URL est accessible
+	def testURLAccessible(self, url):
+		pass
+	
+	#Test le format du fichier
+	def testFormatFichier(self, fichier):
+		pass
+
+if __name__ == '__main__':
+	try:
+		fenetreP = FenetrePrincipale()
+		gtk.main()
+	except KeyboardInterrupt:
+		pass
